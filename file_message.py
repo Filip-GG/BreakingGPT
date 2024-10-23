7ed044945732d1674c631136d84a8c13272c8be6
Filip-GG <filiczhuk@gmail.com>
2024-10-23 18:31:51 +0300

    modified:   README.md 	renamed:    pred_main.ipynb -> YandexGPT.ipynb 	new file:   __pycache__/file_message.cpython-311.pyc 	modified:   __pycache__/localEmbeddings.cpython-311.pyc 	new file:   __pycache__/pdf_convector.cpython-311.pyc 	modified:   config.json 	new file:   dataset/chasto-zadavaemye-voprosy.pdf 	new file:   dataset/frmo_rukovodstvo_polzovatelya.pdf 	new file:   "dataset/\320\230\320\275\321\201\321\202\321\200\321\203\320\272\321\206\320\270\321\217 \320\277\320\276 \321\200\320\260\320\261\320\276\321\202\320\265 \321\201 \320\242\320\242\320\241 \320\255\320\220.pdf" 	new file:   "dataset/\320\230\320\275\321\201\321\202\321\200\321\203\320\272\321\206\320\270\321\217_\320\277\320\276\320\273\321\214\320\267\320\276\320\262\320\260\321\202\320\265\320\273\321\217_\320\277\320\276_\321\200\320\260\320\261\320\276\321\202\320\265_\320\262_\321\201\320\270\321\201\321\202\320\265\320\274\320\265_Naumen_Service_Desk.pdf" 	new file:   "dataset/\320\240\321\203\320\272\320\276\320\262\320\276\320\264\321\201\321\202\320\262\320\276_\320\277\320\276\320\273\321\214\320\267\320\276\320\262\320\260\321\202\320\265\320\273\321\217_\320\241\320\265\321\200\320\262\320\270\321\201 \320\277\320\276 \321\200\320\265\320\263\320\270\321\201\321\202\321\200\320\260\321\206\320\270\320\270 \320\264\320\265\320\272\320\273\320\260\321\200\320\260\321\206\320\270\320\270\314\206 \320\223\320\236\320\241\320\242 \320\240.pdf" 	new file:   dataset_input/frmo_rukovodstvo_polzovatelya.docx 	new file:   "dataset_input/\320\230\320\275\321\201\321\202\321\200\321\203\320\272\321\206\320\270\321\217 \320\277\320\276 \321\200\320\260\320\261\320\276\321\202\320\265 \321\201 \320\242\320\242\320\241 \320\255\320\220.pdf" 	modified:   "dataset_input/\320\240\321\203\320\272\320\276\320\262\320\276\320\264\321\201\321\202\320\262\320\276_\320\277\320\276\320\273\321\214\320\267\320\276\320\262\320\260\321\202\320\265\320\273\321\217_\320\241\320\265\321\200\320\262\320\270\321\201 \320\277\320\276 \321\200\320\265\320\263\320\270\321\201\321\202\321\200\320\260\321\206\320\270\320\270 \320\264\320\265\320\272\320\273\320\260\321\200\320\260\321\206\320\270\320\270\314\206 \320\223\320\236\320\241\320\242 \320\240.docx" 	new file:   file_message.py 	modified:   main.ipynb 	new file:   test_main.ipynb 	new file:   vectorstores/index.faiss 	new file:   vectorstores/index.pkl

---
+++ file_message.py
@@ -0,0 +1,63 @@
+import os
+import shutil
+import pdf_convector
+
+# Получение списка директорий
+def dataset_listdir(patch):
+    return os.listdir(patch)
+
+# набор ф-ций для получения списка файлов типа docx, doc, pdf
+def list_docx_file(patch):
+    return list(
+        filter(
+            lambda item:
+            item[-5:] == '.docx',
+            dataset_listdir(patch)
+        )
+    )
+
+def list_doc_file(patch):
+    return list(
+        filter(
+            lambda item:
+            item[-4:] == '.doc',
+            dataset_listdir(patch)
+        )
+    )
+
+def list_pdf_file(patch):
+    return list(
+        filter(
+            lambda item:
+            item[-4:] == '.pdf',
+            dataset_listdir(patch)
+        )
+    )
+
+# ф-ция копирывания
+def copy(path, out_path):
+    try:
+        shutil.copy(path, out_path)
+        return True
+    except:
+        return 'Error: copy file'
+
+# Переводить все в pdf
+def to_all_pdf(path, out_path):
+    try:
+        all = dataset_listdir(path)
+        print(list(map(lambda item: pdf_convector.doc_to_pdf(item, out_path), list(map(lambda item: path+'/'+item, list_doc_file(path))))))
+        print(list(map(lambda item: pdf_convector.docx_to_pdf(item, out_path), list(map(lambda item: path+'/'+item, list_docx_file(path))))))
+        print(list(map(
+            lambda item:
+            copy(item[0], item[1]),
+            list(
+                zip(
+                    list(map(lambda item: path+'/'+item, list_pdf_file(path))),
+                    list(map(lambda item: out_path+'/'+item, list_pdf_file(path)))
+                )
+            )
+            )))
+        return True
+    except:
+        return 'Error: all to pdf'    
