725ae749e6117c88ff87f82a076941dccd776aa3
Filip-GG <filiczhuk@gmail.com>
2024-10-30 11:53:21 +0300

    modified:   API.py

--- API.py
+++ API.py
@@ -1,15 +1,35 @@
 from fastapi import FastAPI, Request
 from pydantic import BaseModel
-from model import query
 
 app = FastAPI()
 config = 'config.json'
 
 class Query(BaseModel):
+    type : str
     query : str
+    """
+    type - переменная типа запроссв{
+        query - запрос в llm
+        restart - рестарт системы
+        start_rog - запуск rog обуения
+        load_file - загрузка файла
+        delete_file - удаление файла
+        }
+    """
 
+query_type = ["query", "restart", "start_rog", "load_file", "delete_file"]
+
+# Запрос
 @app.post('/')
-async def read_root(data: Query):
-    out = query(data.query, config)
-    return out
+async def query(data: Query):
+    if data.type == query_type[0]:
+        from model import query
+        out = query(data.query, config)
+        return out
+    return "У вас не достаточно прав для использывавания."
 
+@app.post('/admin')
+async def admin_qery(data: Query):
+    if data.type == query_type[0]:
+        return query(data)
+    
